a29f1035c7b9a57d118caf26ef847fd2eb15a39c
Lana Ramadan <lanamramadan@gmail.com>
2023-09-13 23:50:53 -0700

    Drag and Drop

---
+++ drag-and-drop/script.js
@@ -0,0 +1,28 @@
+var lists = document.getElementsByClassName("list");
+var rightBox = document.getElementById("right");
+var leftBox = document.getElementById("left");
+
+for (item of lists) {
+    item.addEventListener("dragstart", function(e){
+        var selected = e.target;
+
+        rightBox.addEventListener("dragover", function(e){
+            e.preventDefault();
+        });
+
+        rightBox.addEventListener("drop", function(e){
+            rightBox.appendChild(selected);
+            selected = null;
+        });
+
+        leftBox.addEventListener("dragover", function(e){
+            e.preventDefault();
+        });
+
+        leftBox.addEventListener("drop", function(e){
+            leftBox.appendChild(selected);
+            selected = null;
+        });
+
+    })
+}
